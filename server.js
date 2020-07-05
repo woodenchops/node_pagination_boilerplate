6b71fda0885e50e36ad9faaf795572677d7c62bc
Kristian JT Woods <kris_woods@hotmail.co.uk>
2020-07-05 11:07:31 +0100

    update payload data with previous and next objects

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require('express');
-const e = require('express');
 
 const app = express();
 
 const PORT = 5000;
 
-const data = [
+const payload = [
     {user: 1},
     {user: 2},
     {user: 3},
@@ -18,21 +17,49 @@ const data = [
     {user: 10}
 ];
 
-app.get('/users', (req, res) => {
+// middleware 
 
-    const page = parseInt(req.query.page);
-    const limit = parseInt(req.query.limit);
+const paginatedData = (model) => {
+    return (req, res, next) => {
 
-    const startIndex = (page - 1) * limit;
-    const endIndex = page * limit;
+        const page = parseInt(req.query.page);
+        const limit = parseInt(req.query.limit);
+    
+        const startIndex = (page - 1) * limit;
+        const endIndex = page * limit;
+    
+        const results = {};
+    
+        if(startIndex > 0) {
+    
+            results.previous = {
+                page: page - 1,
+                limit: limit
+            };
+    
+        }
+    
+        if(endIndex < model.length) {
+               
+            results.next = {
+                page: page + 1,
+                limit: limit
+            };
+    
+        }
+        // if query params arent there, just return full array
+        results.data = (limit && page) ? model.slice(startIndex, endIndex) : model;
+    
+        res.status(200).json({
+            payload: results
+        });
 
-    // if "limit" is undefined, then result will equal all the data
-    const result = (limit) ? data.slice(startIndex, endIndex) : data;
+        next();
 
-    res.status(200).json({
-        payload: result
-    });
-});
+    };
+};
+
+app.get('/users', paginatedData(payload), (req, res) => {});
 
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
